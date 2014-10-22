d1451ccb8b505bfa29152e99534bd59af2fea226
Stephen <scherrer@hawaii.edu>
2014-10-21 14:28:11 -1000

    WorkingDatabaseForAllCode 1.7
    28 August 2014
    
    Updates in version 1.7
    -Added tagging date and tag battery death columns
    -Updated receiver location indexes

--- WorkingDatabaseForAllCode.m
+++ WorkingDatabaseForAllCode.m
@@ -14,7 +14,8 @@ dbstop if error
  
 %%%%%%%%Building ReceiverDates Matrix%%%%%%%%
 
-%%%FOR BUILDING RECEIVERDATES MATRIX FROM THE DEPLOYMENT RECOVERY LOG
+%%%FOR BUILDING RECEIVERDATES MATRIX FROM THE 
+%RECOVERY LOG
 %%WRITTEN 3 JANUARY 2014 IN A COFFEE SHOP IN CAMPBELL BY STEPHEN SCHERRER
 
 %%NOTES: 
@@ -43,8 +44,16 @@ dbstop if error
     TemporaryReceiverDates=[STEVES_ARBITRARY_LOCATION,VR2_SERIAL_NO,AdjustedDeploymentDates, AdjustedRecoveryDates, Lat_deg, Lat_min, Lon_deg, Lon_min];
     Addendum=nan(length(TemporaryReceiverDates),2);
     TemporaryReceiverDates=[TemporaryReceiverDates, Addendum];
-    TemporaryReceiverDates(:,9)=TemporaryReceiverDates(:,5)+(TemporaryReceiverDates(:,6)./(60));
-    TemporaryReceiverDates(:,10)=TemporaryReceiverDates(:,7)+(TemporaryReceiverDates(:,8)./(60));
+    if TemporaryReceiverDates(:,5)>1
+        TemporaryReceiverDates(:,9)=TemporaryReceiverDates(:,5)+(TemporaryReceiverDates(:,6)./(60));
+    else
+        TemporaryReceiverDates(:,9)=TemporaryReceiverDates(:,5)-(TemporaryReceiverDates(:,6)./(60));
+    end
+    if TemporaryReceiverDates(:,7)>1
+        TemporaryReceiverDates(:,10)=TemporaryReceiverDates(:,7)+(TemporaryReceiverDates(:,8)./(60));
+    else
+        TemporaryReceiverDates(:,10)=TemporaryReceiverDates(:,7)-(TemporaryReceiverDates(:,8)./(60));
+    end
     
     
     
@@ -89,20 +98,38 @@ dbstop if error
     %%Column 1=Tag ID
     %%Column 2=Date&Time
     %%Column 3=Reciever ID
-    %%Column 4=Reciever Location (1=Barber Flats, 2=Base3rdFinger, 3=Diamond Head
-        %%Kaena Pocket, 4=Dropoff Inside, 5=First Finger, 6=Haleiwa,7=Kaena Pocket,
-        %%8=Kahuku, 9=Makapuu In BFRA, 10=Makapuu North, 11=Makapuu South,
-        %%12=Marine Corps Base, 13=Pinnacle South, 14=Powerplant, 
-        %%15=South of Ko Olina,16=South Tip, 17=SWAC, 18=The Mound, 19=Waianae,
-        %%20=Cross Seamount, 21=Botcam Crew)
+    %%Column 4=Reciever Location (
+        %OAHU RECEIVERS
+            %1=Haleiwa, 
+            %2=Kahuku, 
+            %3=Marine Corps Base,
+            %4=Makapuu North, 
+            %5=Makapuu in BRFA, 
+            %6=Makapuu South, 
+            %7=Diamond Head,
+            %8=SWAC, 
+            %9=Barber's Flats, 
+            %10=Ko'olina, 
+            %11=Power Plant, 
+            %12=Waianae,
+            %13=Kaena, 
+        %Penguin Banks Receivers
+            %100=First Finger
+            %101=Dropoff Inside
+            %102=The Mound
+            %103=Pinnacle South
+            %104=Base 3rd Finger
+            %105=South Tip
+        %Cross Seamount Recievers=Receiver numbers 200-210
     %%Column 5=Species (1=Ehu, 2=Opaka, 3=Dogfish, 4=Ges, 5=Kale, 6=Sandbar, 0=unknown)
     %%Column 6=Size (cm)
     %%Column 7=Sex (1=Female, 2=Male, 0=Unknown)
     %%Column 8=(Created in following section)=Presence in BRFA or out of BRFA.
         %%1 indicates individual detected in BAFR, 0 indicates detection outside
-        %%BRFA
+        %%BRFA 
+    %%Column 9=Date Tag Was Deployed
+    %%Column 10=Date Tag is Expected to Die
         
-
 
 
 
@@ -191,35 +218,40 @@ end
 %%%%%COLUMN OF Bottomfish? %% Previous loop removes this data but at what
 %%%%%cost? OH THE HUMANITY!
 
+
+
+
+%%%This Dead Fish bit didn't work. silenced in version 1.7
+
 %%accounting for dead fish
 %using dead fish to analyze receiver temporal data
-DeadFish=BottomFish(BottomFish(:,1)==37969,:); %pulls out detections for fish 37969
-DeadFishTimeBins=zeros(2,25); %makes matrix for times 24 hour days (extra bin for 24th hour to be removed later. 
-DeadFishTimeBins(1,:)=linspace(0,1,25); %creates 25 time bins for counting hours
-DeadFishDates=round(max(DeadFish(:,2)+.5))-1:round(min(DeadFish(:,2)+.5)-1); %for determining days of study period from earliest detection date to latest
-DeadFishDateMatrix=zeros(round(max(DeadFish(:,2))-min(DeadFish(:,2))+.5),DeadFishTimeBins); %makes a matrix 25 columns long and length of number of days in study
-for i=min(DeadFish(:,2)):max(DeadFish(:,2)); %indexes total nubmer of days dead fish transmitter has been transmitting
-    DeadFishDateTimeBins=DeadFish(sum((DeadFish(:,2)>i-1)+(DeadFish(:,2)<i))==2,:); %pulls records associated with a single date
-    for a=2:length(DeadFishDateTimeBins);%indexes those records that occured that day
-         DeadFishTimes=DeadFishDateTimeBins(:,2)-(round(DeadFishDateTimeBins(:,2)+.5)-1); %subtracts full date and time from just the date to leave just time data
-         DeadFishTimeDateMatrix(i,a)=sum(DeadFishTimes>=DeadFishTimeBins(1,a-1)+DeadFishTimes<DeadFishTimeBins(a)==2)./length(DeadFishDateTimeBins); %calculates frequency of detections of dead fish by hour and places them in appropriate bins as a fraction of 1
-         [r,~]=size(DeadFishTimeDateMatrix); %finds number of dates in DeadFishTimeMatrix
-         DeadFishTimes(2,a)=sum(DeadFishTimeDateMatrix(:,a))./r; %Divides summed detection rates by total number of days to return a fraction of 1 again.
-    end
-end
-DeadFishTimeDateMatrix(:,25)=[]; % removes bin associated with hour 25 since that doesn't exist
-p=anova1(DeadFishTimeDateMatrix); %performs 1 way ANOVA on daily detection ratios 
-DeadFishTimeBins(:,25)=[];%removes 25th hour bin because there is no 25th hour...
-figure %plots new figure
-hist(DeadFishTimes(2,:),DeadFishTimes(1,:)); %plots histogram of detection frequency where DeadFishTimes(2,:) is proportion of detections for an hour bin and DeadFishTimes(1,:) is the hour bin. 
-datetick %adds hour ticks
-xlabel('Hour Bin') %labels x axis
-ylabel('Frequency of Received Transmissions') %labels y axis
-title('Percentage of Transmissions Received from a Tag in a Very Dead Fish By the Hour They Occured') %titles figure
-mTextBox=uicontrol('style','text'); %creates a text box
-TextBoxText=['One Way Anova P=',num2str(p)];
-set(mTextBox,'string',TextBoxText)%creates a text box with anova results
-print('Figure 1')
+%DeadFish=BottomFish(BottomFish(:,1)==37969,:); %pulls out detections for fish 37969
+%DeadFishTimeBins=zeros(2,25); %makes matrix for times 24 hour days (extra bin for 24th hour to be removed later. 
+%DeadFishTimeBins(1,:)=linspace(0,1,25); %creates 25 time bins for counting hours
+%DeadFishDates=round(min(DeadFish(:,2)+.5))-1:round(max(DeadFish(:,2)+.5)-1); %for determining days of study period from earliest detection date to latest
+%DeadFishTimeDateMatrix=zeros(round(max(DeadFish(:,2))-min(DeadFish(:,2))+.5),length(DeadFishTimeBins(1,:))); %makes a matrix 25 columns long and length of number of days in study
+%for i=min(DeadFish(:,2)):max(DeadFish(:,2)); %indexes total nubmer of days dead fish transmitter has been transmitting
+%    DeadFishDateTimeBins=DeadFish(sum((DeadFish(:,2)>i-1)+(DeadFish(:,2)<i))==2,:); %pulls records associated with a single date
+%    for a=2:length(DeadFishDateTimeBins);%indexes those records that occured that day
+%         DeadFishTimes=DeadFishDateTimeBins(:,2)-(round(DeadFishDateTimeBins(:,2)+.5)-1); %subtracts full date and time from just the date to leave just time data
+%         DeadFishTimeDateMatrix(i,a)=sum(DeadFishTimes>=DeadFishTimeBins(1,a-1)+DeadFishTimes<DeadFishTimeBins(a)==2)./length(DeadFishDateTimeBins); %calculates frequency of detections of dead fish by hour and places them in appropriate bins as a fraction of 1
+%         [r,~]=size(DeadFishTimeDateMatrix); %finds number of dates in DeadFishTimeMatrix
+%         DeadFishTimes(2,a)=sum(DeadFishTimeDateMatrix(:,a))./r; %Divides summed detection rates by total number of days to return a fraction of 1 again.
+%    end
+%end
+%DeadFishTimeDateMatrix(:,25)=[]; % removes bin associated with hour 25 since that doesn't exist
+%p=anova1(DeadFishTimeDateMatrix); %performs 1 way ANOVA on daily detection ratios 
+%DeadFishTimeBins(:,25)=[];%removes 25th hour bin because there is no 25th hour...
+%figure %plots new figure
+%hist(DeadFishTimes(2,:),DeadFishTimes(1,:)); %plots histogram of detection frequency where DeadFishTimes(2,:) is proportion of detections for an hour bin and DeadFishTimes(1,:) is the hour bin. 
+%datetick %adds hour ticks
+%xlabel('Hour Bin') %labels x axis
+%ylabel('Frequency of Received Transmissions') %labels y axis
+%title('Percentage of Transmissions Received from a Tag in a Very Dead Fish By the Hour They Occured') %titles figure
+%mTextBox=uicontrol('style','text'); %creates a text box
+%TextBoxText=['One Way Anova P=',num2str(p)];
+%set(mTextBox,'string',TextBoxText)%creates a text box with anova results
+%print('Figure 1')
 
 %removing the dead fish
 BottomFish=BottomFish(BottomFish(:,1)~=37969,:); %%This fish is very dead right next to a receiver.
@@ -321,7 +353,7 @@ end
 
 clearvars h r a i
     
-[A,L]=find(BottomFish(:,4)==9);
+[A,L]=find(BottomFish(:,4)==5);
 if A>0;
    disp ('finally found a fish in BRFA!, follow the following fish through the rest of this process')
     L(1)
@@ -332,9 +364,11 @@ end
 
 %%If an individual is in BRFA column 6=1, if outside, column 6=0
 for i=1:Y
-    if BottomFish(i,4)==9
+    if BottomFish(i,4)==5
         BottomFish(i,8)=1;
-    elseif BottomFish(i,4)~=9
+    elseif BottomFish(i,4)~=101
+        BottomFish(i,8)=1;
+    else
         BottomFish(i,8)=0;
     end
 end
@@ -557,7 +591,56 @@ OahuDatabase=BFT(Index,:); %only ones make up OahuDatabase
 
 BottomFish=OahuDatabase(isnan(OahuDatabase(:,5))==0,:); %if There is a number associated with species for OahuDatabase, it counts as BottomFish
 
+%%%%Start Added April 28 2014
+
+%Creating TagDate Variable
+Date=(datenum(2012,08,18)-min(Date))+Date;
+TagDate=[VemTagCode,datenum(Date)];
+%Creating a 9th and 10th Column for BottomFish Workspace 9th column for Tag Date, 10th for anticipated transmitter death.
+Addendum=nan(length(BottomFish),2); %creates additional columns for BottomFish variable
+BottomFish=[BottomFish,Addendum]; %adds these variables to Bottomfish matrix
+clear Addendum %clears Addendum Variable
+%%%Replacing zeros in column 9 of BottomFish with Tagging Date
+for i=1:length(TagDate); %indexes previously created TagDate variable
+    for b=1:length(BottomFish); %indexes all monitor detections
+        if TagDate(i,1)==BottomFish(b,1); %if Tag IDs match
+          BottomFish(b,9)=TagDate(i,2); %Tagging Date written to column 9 of BottomFish Variable
+        end
+    end
+end
+clear i b;
+%%In this project, tags have been coded as follows:
+
+%%Tag Code Range     Average Seconds     Batery life (Days)
+%%57371-57470        150                 450
+%%37935-37985        180                 539
+%%52142-52161        150                 450
+
+%Calculating Tag Death
+for i=1:length(BottomFish); %indexes BottomFish matrix
+	if BottomFish(i,1)>=57371 && BottomFish(i,1)<=57470;
+		BottomFish(i,10)=BottomFish(i,9)+450;
+	elseif BottomFish(i,1)>=37935 && BottomFish(i,1)<=37985;
+		BottomFish(i,10)=BottomFish(i,9)+539;
+	elseif BottomFish(i,1)>=52142 && BottomFish(i,1)<=52161;
+		BottomFish(i,10)=Bottomfish(i,9)+450;
+	end
+end
+
+%Breaking Out Tags By Species
+
+Ehu=BottomFish(BottomFish(:,5)==1,:);
+Opakapaka=BottomFish(BottomFish(:,5)==2,:);
+Kalekale=BottomFish(BottomFish(:,5)==5,:);
+%Onaga=BottomFish(BottomFish(:,5)==___,:);
+
+%%%%END ADDED SECION April 28
+
 csvwrite('BottomFish.csv',BottomFish);
+csvwrite('Ehu.csv',Ehu);
+csvwrite('Opakapaka.csv',Opakapaka);
+csvwrite('KaleKale.csv',KaleKale);
+%csvwrite('Onaga.csv',Onaga);
 
 %clearvars DetectionsByTagIDThenDate UniqueDetectionsPerFish i NonDuplicateData Dates Tags BFT ReceiverDateAdjustment Latitude Longitude SensorUnit i SensorValue StationName TransmitterName TransmitterSerial B  FT BFD IAmTheRemover TagsUnsorted TagsSorted DatesUnsorted DatesSorted i j k r t s a l I J K R T S A L t T Index NonDuplicateData Index TagsSorted TagsUnsorted DatesSorted DatesUnsorted A L l s b i AdjustedReceiverDeploymentsAndRecoveries DateTime FLcm Receiver ReceiverDeploymentsAndRecoveries Sex Sex1 Species StomachEverted TagID Tags TempReceiver TempSex TempSex1 TempTransmitter TemporaryReceiverDates Time Transmitter  VarName1 a ans
 
@@ -570,21 +653,42 @@ toc
     %%Column 1=Tag ID
     %%Column 2=Date&Time
     %%Column 3=Reciever ID
-    %%Column 4=Receiver Location (1=Barber Flats, 2=Base3rdFinger, 3=Diamond Head
-        %%Pocket, 4=Dropoff Inside, 5=First Finger, 6=Haleiwa,7=Kaena Pocket,
-        %%8=Kahuku, 9=Makapuu In BFRA, 10=Makapuu North, 11=Makapuu South,
-        %%12=Marine Corps Base, 13=Pinnacle South, 14=Powerplant, 
-        %%15=South of Ko Olina,16=South Tip, 17=SWAC, 18=The Mound, 19=Waianae,
-        %%20=Cross Seamount, 21=Botcam Crew)
+    %%Column 4=Reciever Location 
+        %OAHU RECEIVERS
+            %1=Haleiwa, 
+            %2=Kahuku, 
+            %3=Marine Corps Base,
+            %4=Makapuu North, 
+            %5=Makapuu in BRFA, 
+            %6=Makapuu South, 
+            %7=Diamond Head,
+            %8=SWAC, 
+            %9=Barber's Flats, 
+            %10=Ko'olina, 
+            %11=Power Plant, 
+            %12=Waianae,
+            %13=Kaena, 
+        %Penguin Banks Receivers
+            %100=First Finger
+            %101=Dropoff Inside
+            %102=The Mound
+            %103=Pinnacle South
+            %104=Base 3rd Finger
+            %105=South Tip
     %%Column 5=Species (1=Ehu, 2=Opaka, 3=Dogfish, 4=Ges, 5=Kale, 6=Sandbar, NaN=unknown)
     %%Column 6=Size (cm) if=NaN, information Unknown/Unavailable
     %%Column 7=Sex (1=Female, 2=Male, NaN=Unknown/Unavailable)
     %%Column 8=(Created in following section)=Presence in BRFA or out of BRFA.
         %%1 indicates individual detected in BAFR, 0 indicates detection outside
         %%BRFA
+    %%Column 9=Date Tag Was Deployed
+    %%Column 10=Date Tag is Expected to Die
         
         
 %%%%%%%%%%%%%%%%%%%%Version Updates%%%%%%%%%%%%%%%%%%%%%%
+%%Updates in V_1.7
+    %%Added Tag date and tag death columns
+    %%Updated receiver location indexes
 %%Updates in V_1.6
     %%in V_1.5, tried to automatically import data files but this proved
     %%problematic so it has been removed until it can be debugged. Added
